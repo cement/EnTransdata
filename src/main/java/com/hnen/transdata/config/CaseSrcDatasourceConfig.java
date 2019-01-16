920128a7b35ee31487469bc85053e6e6e88cd9fb
cement <32463636@qq.com>
2019-01-16 15:28:32 +0800

    新硬盘第一次提交

--- src/main/java/com/hnen/transdata/config/CaseSrcDatasourceConfig.java
+++ src/main/java/com/hnen/transdata/config/CaseSrcDatasourceConfig.java
@@ -6,6 +6,7 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,12 +19,12 @@ import javax.sql.DataSource;
 @MapperScan(basePackages = "com.hnen.transdata.mapper.casesrc" , sqlSessionTemplateRef = "caseSrcSqlSessionTemplate")
 public class CaseSrcDatasourceConfig {
 
-//    @ConfigurationProperties(prefix = "datasource.src.case.druid")
+    @ConfigurationProperties(prefix = "datasource.src.case.druid")
     @Bean(name = "caseSrcDataSource")
     public DataSource getSrcDataSource() {
         return DataSourceBuilder.create().type(com.alibaba.druid.pool.DruidDataSource.class)
-                .driverClassName("oracle.jdbc.driver.OracleDriver").password("hdpass").username("tyufu")
-                .url("jdbc:oracle:thin:@//222.240.140.90:65231/prod")
+//                .driverClassName("oracle.jdbc.driver.OracleDriver").password("hdpass").username("tyufu")
+//                .url("jdbc:oracle:thin:@//222.240.140.90:65231/prod")
                 .build();
     }
 
